e0af14d437ee21cc876df56161dee347c4d8f74d
karavatigopinath <158138570+karavatigopinath@users.noreply.github.com>
2024-07-05 13:06:55 +0530

    Simple Curd with express

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from 'express'
+const app = express();
+app.use(express.json())
+const port = 3002;
+app.get('/',(req,res)=>{
+  return res.send('this is response from express..!')
+})
+
+let teaData = [];
+let nextIndex = 1;
+app.post('/teas',(req,res)=>{
+    const {name, price} = req.body;
+    const newTea = {id:nextIndex++,name,price};
+    teaData.push(newTea);
+    return res.status(201).send(teaData);
+})
+app.get('/teas',(req,res)=>{
+  return res.status(200).send(teaData)
+})
+app.get('/teas/:id',(req,res)=>{
+    const tea = teaData.find(t=>t.id === parseInt(req.params.id));
+    if(!tea){
+        return res.status(404).send("404 Not found");
+    }
+    res.status(200).send(tea);
+})
+app.put('/teas/:id',(req,res)=>{
+    const tea = teaData.find(t=>t.id === parseInt(req.params.id));
+    if(!tea){
+        return res.status(404).send("Index  404 Not found.!");
+    }
+    const {name,price} = req.body;
+    tea.name = name;
+    tea.price = price;
+   return res.status(201).send(tea);
+})
+app.delete('/teas/:id',(req,res)=>{
+    const index = teaData.findIndex(t=>t.id === parseInt(req.params.id));
+    if(index === -1){
+        return res.status(404).send("Not found.!");
+    }
+    teaData.splice(index,1);
+     res.status(201).send("Data deleted..!")
+})
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
+})
